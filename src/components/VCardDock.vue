baffe8c673334f698437b60fc97ce9b8c3867eec
Claudio Brogliato <cbrogliato@gmail.com>
2021-06-01 16:54:06 +0200

    Added VDockableCard VCardDock and VSlottedSelect

---
+++ src/components/VCardDock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card-dock w-100 d-flex flex-column flex-xl-row">
+    <div class="col">
+      <slot name="leftCol">
+      </slot>
+    </div>
+    <div class="col" v-if="hasRightCol()">
+      <slot name="rightCol">
+      </slot>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class CardDock extends Vue {
+  hasRightCol() {
+    return !!this.$slots.rightCol || !!this.$scopedSlots.rightCol;
+  }
+}
+</script>
